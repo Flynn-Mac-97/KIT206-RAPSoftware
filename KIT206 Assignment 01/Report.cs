03e2ea4b95e0a5dc132dbc6fa698aaa77e7233dd
jackook1 <152351397+jackook1@users.noreply.github.com>
2023-12-07 17:11:09 +0900

    Report class

---
+++ KIT206 Assignment 01/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_Assignment_01
+{
+    class Report
+    {
+        public ResearcherPerformance performance { get; set; }
+
+    }
+}
