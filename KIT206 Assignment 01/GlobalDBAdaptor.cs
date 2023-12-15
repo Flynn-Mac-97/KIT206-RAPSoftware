cf3c74a1692a9aeb77653ab4aeda4b1d0ea26251
Flynn-Mac-97 <103421936+Flynn-Mac-97@users.noreply.github.com>
2023-12-15 11:55:09 +1100

    Added database connection to the global db adaptor, and added the supplied functions given by bilal to test connection.

--- KIT206 Assignment 01/GlobalDBAdaptor.cs
+++ KIT206 Assignment 01/GlobalDBAdaptor.cs
@@ -1,13 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace KIT206_Assignment_01
 {
     class GlobalDBAdaptor
     {
+        //Note that ordinarily these would (1) be stored in a settings file and (2) have some basic encryption applied
+        private const string db = "kit206";
+        private const string user = "kit206";
+        private const string pass = "kit206";
+        private const string server = "alacritas.cis.utas.edu.au";
+
+        private MySqlConnection conn;
+
+        //Constructor for the DB adaptor, creates a connection to the database
+        public GlobalDBAdaptor()
+        {
+            //create a connection to the database
+            string connectionString = String.Format("Database={0};Data Source={1};User Id={2};Password={3}", db, server, user, pass);
+            conn = new MySqlConnection(connectionString);
+
+            this.ReadData();
+            this.ReadIntoDataSet();
+        }
+        /*
+         * Using the ExecuteReader method to select from a single table
+         */
+        public void ReadData()
+        {
+            MySqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                MySqlCommand cmd = new MySqlCommand("select given_name, family_name from researcher", conn);
+
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    //This illustrates how the raw data can be obtained using an indexer [] or a particular data type can be obtained using a GetTYPENAME() method.
+                    //Console.WriteLine("{0} {1}", rdr[0], rdr.GetString(1));
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /*
+        * Using the ExecuteReader method to select from a single table
+        */
+        public void ReadIntoDataSet()
+        {
+            try
+            {
+                var researcherDataSet = new DataSet();
+                var researcherAdapter = new MySqlDataAdapter("select * from researcher", conn);
+                researcherAdapter.Fill(researcherDataSet, "researcher");
+
+                foreach (DataRow row in researcherDataSet.Tables["researcher"].Rows)
+                {
+                    //Again illustrating that indexer (based on column name) gives access to whatever data
+                    //type was obtained from a given column, but can call ToString() on an entry if needed.
+                    //Console.WriteLine("Name: {0} {1}", row["given_name"], row["family_name"].ToString());
+                }
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+        /*
+        * Using the ExecuteScalar method
+        * returns number of records
+        */
+        public int GetNumberOfRecords()
+        {
+            int count = -1;
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command
+                MySqlCommand cmd = new MySqlCommand("select COUNT(*) from researcher", conn);
+
+                // 2. Call ExecuteScalar to send command
+                // This convoluted approach is safe since cannot predict actual return type
+                count = int.Parse(cmd.ExecuteScalar().ToString());
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return count;
+        }
         //Demo function for testing functionality with no connection to a DB
         // a complete function which works with DB data should work in exactly the same way but instead not use random values.
         public List<Researcher> GenerateDummyResearchers(int amount)
@@ -77,3 +191,4 @@ namespace KIT206_Assignment_01
         }
     }
 }
+
