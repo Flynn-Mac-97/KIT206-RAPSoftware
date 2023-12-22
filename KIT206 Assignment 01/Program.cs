e784480b571a09caab4bcf61d9eb83e148d64d33
Flynn-Mac-97 <flynn.mcmurray@utas.edu.au>
2023-12-22 14:27:49 +1100

    Added WPF UI to project

--- KIT206 Assignment 01/Program.cs
+++ KIT206 Assignment 01/Program.cs
@@ -6,23 +6,23 @@ namespace KIT206_Assignment_01
 {
     class Program
     {
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             ResearchController researchController = new ResearchController();
-            GlobalDBAdaptor database = new GlobalDBAdaptor();
+            //GlobalDBAdaptor database = new GlobalDBAdaptor();
 
             //researchController.FetchDummyResearchers(16, database);
 
-            researchController.FetchResearcherList(database);
+            //researchController.FetchResearcherList(database);
 
             Console.WriteLine("All Researchers:\n");
-            researchController.PrintAllResearchers();
+            //researchController.PrintAllResearchers();
 
             Console.WriteLine("Filtered Researchers:\n");
-            researchController.FilterResearcher("", EmploymentLevel.LECTURER);
-            researchController.PrintAllFilteredResearchers();
+            //researchController.FilterResearcher("", EmploymentLevel.LECTURER);
+            //researchController.PrintAllFilteredResearchers();
 
             Console.WriteLine("Finished Generating Researchers");
-        }
+        }*/
     }
 }
